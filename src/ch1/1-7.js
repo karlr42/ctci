c380b1925379259a44d7f4785722b28e82de59b9
Karl Reid <karlr42@gmail.com>
2017-11-14 23:49:17 +0000

    ch1-7

--- src/ch1/1-7.js
+++ src/ch1/1-7.js
@@ -1,5 +1,27 @@
 var rotateMatrix = function (matrix, n){
-	
+	/*
+		- transpose matrix(make rows into columns by diagonal-left-down swapping)
+		- then swap the order of columns to get the result
+	*/
+	for(var i = 0 ; i < n; i++){
+		// row-by-row transpose
+		for(var j = i+1; j < n; j++){
+			// diaognally swap 
+			let temp = matrix[i][j];
+      		matrix[i][j] = matrix[j][i];
+      		matrix[j][i] = temp;
+		}
+	}
+	// swap leftmost row cells with rightmost cells
+	for(var i = 0 ; i < n; i++){
+		for(var j = 0 ; j < n/2; j++){
+			let temp = matrix[i][j];
+			matrix[i][j] = matrix[i][n-1-j];
+			matrix[i][n-1-j] = temp;
+		}
+	}
+	return matrix;
+
 };
 
 module.exports = {rotateMatrix};
