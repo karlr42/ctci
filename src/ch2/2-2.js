675e27cab0554bf00bfebe596b22fa76f504f123
Karl Reid <karlr42@gmail.com>
2017-11-16 22:50:51 +0000

    ch2-2 alt solution

--- src/ch2/2-2.js
+++ src/ch2/2-2.js
@@ -16,4 +16,22 @@ let getKthFromLast = function(list, k){
 	return node.val;
 };
 
+var getKthFromLastTwoPtrs = function(list, k){
+	/*
+		run two pointers, one that is k behind
+		the other. Once the first has reached the end,
+		the second one is pointing at the result.
+	*/
+	var head = list, behind = list;
+	while(head != null){
+		head = head.next;
+		if(k >= 0){ //give head a k 'headstart'
+			k--;
+		}else{
+			behind = behind.next;
+		}
+	}
+	return behind.val;
+}
+
 module.exports = {getKthFromLast};
