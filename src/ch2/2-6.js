b294d967e165a51bd4933c3c87c051b1d1857997
Karl Reid <karlr42@gmail.com>
2017-11-19 14:48:20 +0000

    ch2-6 alt approach

--- src/ch2/2-6.js
+++ src/ch2/2-6.js
@@ -1,4 +1,4 @@
-let isPalindromeOddElemCount = function(list){
+let isPalindrome = function(list){
 	var map = {};
 	for(var node = list; node != null; node = node.next){
 		var v = node.val;
@@ -20,4 +20,35 @@ let isPalindromeOddElemCount = function(list){
 	return oddCount < 2;
 };
 
-module.exports = {isPalindromeOddElemCount};
+var isPalindromeStack = function(list){
+	/*
+		two pointers- one normal, one twice as fast 
+		first one pushes to a stack as it proceeds
+		when second reaches the end, first is at the middle
+		then first keeps going and pops off stack to compare
+
+		has good performance- O(n/2) + however long to reach
+		first difference, so O(n) at the worst.
+		but - this can't be easily modified for linked lists that
+		are strings with punctuation etc. that you need to
+		skip for comparisons, so that's a reason to prefer the other approach.
+		You could pre-process the list first.
+	*/
+	var stack = [];
+	for(var node = list, runner = list; runner != null && runner.next != null; 
+			node = node.next, runner = runner.next.next){
+		stack.push(node.val);
+	}
+	if(runner != null){
+		// ll has odd length, because we skipped to null with a jump of 2
+		node = node.next;
+	}
+	for(; node != null; node = node.next){
+		var a = node.val;
+		var b = stack.pop();
+		if(a !== b) return false;
+	}
+	return true;
+};
+
+module.exports = {isPalindrome, isPalindromeStack};
--- src/ch2/2-6.spec.js
+++ src/ch2/2-6.spec.js
@@ -1,37 +1,59 @@
 var app = require("./2-6.js");
 var linkedList = require("./linkedList.js");
 
-
-for (let fName in app) {
-	let func = app[fName];
-
-	describe("2-6 "+fName,function() {
-		[
-			["m","a","d","a","m","i","m","a","d","a","m"],
-			["A"," ","m","a","n",","," ","a"," ","p","l","a","n",","," ","a"," ",
-				"c","a","t",","," ","a"," ","c","a","n","a","l",";"," ","P","a","n","a","m","a","!"],
-			[0,1,2,1,0],
-			[true,false,true,false,true]
-		].forEach(arg => {
-			it("find linked list palindromes", function() {
-				assert.equal(true,
-					func(linkedList.arrayToLinkedList(arg)));
-			});
+describe("2-6 isPalindrome",function() {
+	[
+		["m","a","d","a","m","i","m","a","d","a","m"],
+		["A"," ","m","a","n",","," ","a"," ","p","l","a","n",","," ","a"," ",
+			"c","a","t",","," ","a"," ","c","a","n","a","l",";"," ","P","a","n","a","m","a","!"],
+		[0,1,2,1,0],
+		[1,2,3,3,2,1],
+		[true,false,true,false,true]
+	].forEach(arg => {
+		it("finds linked list palindromes", function() {
+			assert.equal(true,
+				app.isPalindrome(linkedList.arrayToLinkedList(arg)));
 		});
-
-		[
-			["m","a","d","a","m","i","m","n","o","t","a","d","a","m"],
-			["A"," ","m","y","n",","," ","a"," ","p","l","a","n",","," ","a"," ",
-				"c","a","t",","," ","a"," ","c","a","n","a","l",";"," ","P","a","n","a","m","a","!"],
-			[0,1,2,2,3,0],
-			[true,false,true,false,true,false]
-		].forEach(arg => {
-			it("return false for not linked list palindromes", function() {
-				assert.equal(false,
-					func(linkedList.arrayToLinkedList(arg)));
-			});
-		});
-
 	});
-}
 
+	[
+		["m","a","d","a","m","i","m","n","o","t","a","d","a","m"],
+		["A"," ","m","y","n",","," ","a"," ","p","l","a","n",","," ","a"," ",
+			"c","a","t",","," ","a"," ","c","a","n","a","l",";"," ","P","a","n","a","m","a","!"],
+		[0,1,2,2,3,0],
+		[1,2,3,4,2,1],
+		[true,false,true,false,true,false]
+	].forEach(arg => {
+		it("returns false for not linked list palindromes", function() {
+			assert.equal(false,
+				app.isPalindrome(linkedList.arrayToLinkedList(arg)));
+		});
+	});
+});
+
+describe("2-6 isPalindromeStack",function() {
+
+	[
+		["m","a","d","a","m","i","m","a","d","a","m"],
+		[0,1,2,1,0],
+		[1,2,3,3,2,1],
+		[true,false,true,false,true]
+	].forEach(arg => {
+		it("stack-approach finds linked list palindromes", function() {
+			assert.equal(true,
+				app.isPalindromeStack(linkedList.arrayToLinkedList(arg)));
+		});
+	});
+
+	[
+		["m","a","d","a","m","i","m","n","o","t","a","d","a","m"],
+		[0,1,2,2,3,0],
+		[1,2,3,4,2,1],
+		[true,false,true,false,true,false]
+	].forEach(arg => {
+		it("stack-approach returns false for not linked list palindromes", function() {
+			assert.equal(false,
+				app.isPalindromeStack(linkedList.arrayToLinkedList(arg)));
+		});
+	});
+});
